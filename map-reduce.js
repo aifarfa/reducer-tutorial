0a7d780e05606de3b7f98020a61858893d63c37b
pasit.r <pasit.rattanathammak@thomsonreuters.com>
2016-06-16 15:36:23 +0700

    re-factor in functional style

--- map-reduce.js
+++ map-reduce.js
@@ -1,41 +1,67 @@
 'use strict'
+const _ = require('lodash')
 
-const reducer = (state, action) => {
-  let nextState = {
-    x: state.x,
-    y: state.y
-  }
+// const reducer = (state, action) => {
+//   let nextState = {
+//     x: state.x,
+//     y: state.y
+//   }
 
-  switch (action.type) {
-    case 'LEFT':
-      nextState.x -= action.value
-      break;
-    case 'RIGHT':
-      nextState.x += action.value
-      break;
-    case 'UP':
-      nextState.y += action.value
-      break;
-    case 'DOWN':
-      nextState.y -= action.value
-      break;
-    default:
-      break;
-  }
-  // console.log(action.type, action.value)
-  console.log(nextState)
-  return nextState;
-}
+//   switch (action.type) {
+//     case 'LEFT':
+//       nextState.x -= action.value
+//       break;
+//     case 'RIGHT':
+//       nextState.x += action.value
+//       break;
+//     case 'UP':
+//       nextState.y += action.value
+//       break;
+//     case 'DOWN':
+//       nextState.y -= action.value
+//       break;
+//     default:
+//       break;
+//   }
+
+//   console.log(nextState)
+//   return nextState;
+// }
+
+const isVertical = action => _.includes(['UP', 'DOWN'], action.type)
+const isHorizontal = action => _.includes(['LEFT', 'RIGHT'], action.type)
+
+const scaleX = action => action.type === 'RIGHT' ? action.value : -action.value
+const scaleY = action => action.type === 'UP' ? action.value : -action.value
 
 const dispatch = (state, actions) => {
+
   let initialState = state || {
     x: 0,
     y: 0
   }
-  return actions.reduce(reducer, initialState)
+
+  const addDistance = (current, value) => current + value
+  const vectorX = actions
+    .filter(isHorizontal)
+    .map(scaleX)
+    .reduce(addDistance, 0)
+
+  const vectorY = actions
+    .filter(isVertical)
+    .map(scaleY)
+    .reduce(addDistance, 0)
+
+  console.log('move X ->', vectorX)
+  console.log('move Y ->', vectorY)
+
+  // return actions.reduce(reducer, initialState)
+  return {
+    x: initialState.x + vectorX,
+    y: initialState.y + vectorY
+  }
 }
 
 module.exports = {
-  dispatch,
-  reducer
+  dispatch
 }
